d0dca8794768115bf05a4fac568a032e552d5af4
K40NN <77363726+K40NN@users.noreply.github.com>
2023-05-24 14:27:00 +0000

    GIT 16:24

--- main.cpp
+++ main.cpp
@@ -1,33 +1,90 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
 
 const int BOARD_SIZE = 16;
 
-void generateBoard() {
-    char board[BOARD_SIZE][BOARD_SIZE];
+class Cell {
+public:
+    char symbol;
 
-    // Initialize the board with empty spaces
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            board[i][j] = ' ';
+    Cell() : symbol(' ') {}
+};
+
+class Wall {
+public:
+    bool isHorizontal;
+
+    Wall(bool horizontal) : isHorizontal(horizontal) {}
+};
+
+class Board {
+private:
+    std::vector<std::vector<Cell>> cells;
+
+public:
+    Board() {
+        cells.resize(BOARD_SIZE, std::vector<Cell>(BOARD_SIZE));
+    }
+
+    void initialize() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                cells[i][j].symbol = ' ';
+            }
         }
     }
 
+    void placeWall(int x, int y, const Wall& wall) {
+        if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+            if (wall.isHorizontal) {
+                cells[x][y].symbol = '-';
+                cells[x + 1][y].symbol = '-';
+            } else {
+                cells[x][y].symbol = '|';
+                cells[x][y + 1].symbol = '|';
+            }
+        }
+    }
+
+    void placeCell(int x, int y, char symbol) {
+        if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE) {
+            cells[x][y].symbol = symbol;
+        }
+    }
+
+    const std::vector<std::vector<Cell>>& getCells() const {
+        return cells;
+    }
+
+    void print() const {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                std::cout << cells[i][j].symbol;
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+void generateBoard(Board& board) {
+    board.initialize();
+
     // Place the outer walls
     for (int i = 0; i < BOARD_SIZE; i++) {
-        board[0][i] = '-';
-        board[BOARD_SIZE - 1][i] = '-';
-        board[i][0] = '|';
-        board[i][BOARD_SIZE - 1] = '|';
+        board.placeWall(0, i, Wall(true));
+        board.placeWall(BOARD_SIZE - 1, i, Wall(true));
+        board.placeWall(i, 0, Wall(false));
+        board.placeWall(i, BOARD_SIZE - 1, Wall(false));
     }
 
     // Place the walls in the middle square
     for (int i = 6; i < 10; i++) {
-        board[6][i] = '-';
-        board[9][i] = '-';
-        board[i][6] = '|';
-        board[i][9] = '|';
+        board.placeWall(6, i, Wall(true));
+        board.placeWall(9, i, Wall(true));
+        board.placeWall(i, 6, Wall(false));
+        board.placeWall(i, 9, Wall(false));
     }
 
     // Generate the walls and angles in each quadrant
@@ -36,21 +93,22 @@ void generateBoard() {
         // Place two outer walls (one horizontal, one vertical)
         int outerWallX = (quadrant % 2 == 0) ? 1 : BOARD_SIZE - 2;
         int outerWallY = (quadrant < 2) ? 1 : BOARD_SIZE - 2;
-        board[outerWallX][outerWallY] = (quadrant % 2 == 0) ? '-' : '|';
+        board.placeWall(outerWallX, outerWallY, Wall(quadrant % 2 == 0));
+        board.placeWall((quadrant % 2 == 0) ? outerWallX + 1 : outerWallX - 1, outerWallY, Wall(quadrant % 2 == 0));
 
-        // Place four angles
+        // Place four angles (two walls each)
         for (int angle = 0; angle < 4; angle++) {
             int angleX, angleY;
             do {
                 angleX = (quadrant % 2 == 0) ? rand() % 4 + 2 : rand() % 4 + 10;
                 angleY = (quadrant < 2) ? rand() % 4 + 2 : rand() % 4 + 10;
-            } while (board[angleX][angleY] != ' ' || board[angleX - 1][angleY] != ' ' ||
-                     board[angleX][angleY - 1] != ' ' || board[angleX - 1][angleY - 1] != ' ');
+            } while (board.getCells()[angleX][angleY].symbol != ' ' || board.getCells()[angleX - 1][angleY].symbol != ' ' ||
+                     board.getCells()[angleX][angleY - 1].symbol != ' ' || board.getCells()[angleX - 1][angleY - 1].symbol != ' ');
 
-            board[angleX][angleY] = (angle % 2 == 0) ? '-' : '|';
-            board[angleX - 1][angleY] = (angle % 2 == 0) ? '-' : '|';
-            board[angleX][angleY - 1] = (angle % 2 == 0) ? '-' : '|';
-            board[angleX - 1][angleY - 1] = (angle % 2 == 0) ? '-' : '|';
+            board.placeWall(angleX, angleY, Wall(angle % 2 == 0));
+            board.placeWall(angleX - 1, angleY, Wall(angle % 2 == 0));
+            board.placeWall(angleX, angleY - 1, Wall(angle % 2 == 0));
+            board.placeWall(angleX - 1, angleY - 1, Wall(angle % 2 == 0));
         }
     }
 
@@ -60,13 +118,13 @@ void generateBoard() {
     do {
         additionalAngleX = (additionalAngleQuadrant % 2 == 0) ? rand() % 4 + 2 : rand() % 4 + 10;
         additionalAngleY = (additionalAngleQuadrant < 2) ? rand() % 4 + 2 : rand() % 4 + 10;
-    } while (board[additionalAngleX][additionalAngleY] != ' ' || board[additionalAngleX - 1][additionalAngleY] != ' ' ||
-             board[additionalAngleX][additionalAngleY - 1] != ' ' || board[additionalAngleX - 1][additionalAngleY - 1] != ' ');
+    } while (board.getCells()[additionalAngleX][additionalAngleY].symbol != ' ' || board.getCells()[additionalAngleX - 1][additionalAngleY].symbol != ' ' ||
+             board.getCells()[additionalAngleX][additionalAngleY - 1].symbol != ' ' || board.getCells()[additionalAngleX - 1][additionalAngleY - 1].symbol != ' ');
 
-    board[additionalAngleX][additionalAngleY] = '-';
-    board[additionalAngleX - 1][additionalAngleY] = '-';
-    board[additionalAngleX][additionalAngleY - 1] = '|';
-    board[additionalAngleX - 1][additionalAngleY - 1] = '|';
+    board.placeWall(additionalAngleX, additionalAngleY, Wall(true));
+    board.placeWall(additionalAngleX - 1, additionalAngleY, Wall(true));
+    board.placeWall(additionalAngleX, additionalAngleY - 1, Wall(false));
+    board.placeWall(additionalAngleX - 1, additionalAngleY - 1, Wall(false));
 
     // Place the robots randomly
     char robots[] = { 'R', 'G', 'B', 'Y' };
@@ -75,21 +133,16 @@ void generateBoard() {
         do {
             robotX = rand() % (BOARD_SIZE - 4) + 2;
             robotY = rand() % (BOARD_SIZE - 4) + 2;
-        } while (board[robotX][robotY] != ' ');
+        } while (board.getCells()[robotX][robotY].symbol != ' ');
 
-        board[robotX][robotY] = robots[i];
-    }
-
-    // Print the board
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            std::cout << board[i][j];
-        }
-        std::cout << std::endl;
+        board.placeCell(robotX, robotY, robots[i]);
     }
 }
 
 int main() {
-    generateBoard();
+    Board board;
+    generateBoard(board);
+    board.print();
+
     return 0;
 }
